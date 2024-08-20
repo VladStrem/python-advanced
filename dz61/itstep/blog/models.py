2c1c938e8e1d2642b78634570fe13a33009b96cb
Vlad <petrenkov785@gmail.com>
2024-08-20 12:19:26 +0200

    Dz [61]

---
+++ dz61/itstep/blog/models.py
@@ -0,0 +1,62 @@
+from django.contrib.auth.models import User
+from django.db import models
+from django.utils import timezone
+
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
+
+
+# Create your models here.
+class Post(models.Model):
+    class Status(models.TextChoices):
+        DRAFT = ('DF', 'Draft')
+        PUBLISHED = ('PB', 'Published')
+
+    title = models.CharField(verbose_name="Назва поста", max_length=255)
+    body = models.TextField()
+    slug = models.SlugField(max_length=255, unique=True)
+
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    status = models.CharField(max_length=2,
+                              choices=Status.choices,
+                              default=Status.DRAFT)
+
+    category = models.ForeignKey("Category", on_delete=models.CASCADE, null=True,
+                                 related_name="posts")
+
+    objects = models.Manager()
+    published = PublishedManager()
+
+    def __str__(self):
+        return f'[{self.pk}] - {self.title}'
+
+    class Meta:
+        ordering = ["publish"]
+        verbose_name_plural = "Публікації"
+        verbose_name = "Публікації"
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=255, default='')
+
+    def __str__(self):
+        return self.title
+
+
+class Rating(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='ratings')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_ratings')
+    rating = models.IntegerField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        unique_together = ('post', 'user')
+
+    def __str__(self):
+        return f"{self.user.username} rated {self.post.title} with {self.rating} stars"
