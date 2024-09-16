9e3907880b2dfe234bde960ffed9b158cfe1a996
Vlad <petrenkov785@gmail.com>
2024-09-16 14:47:49 +0200

    Dz [77]

---
+++ dz77/itstep/account/models.py
@@ -0,0 +1,13 @@
+from django.conf import settings
+from django.contrib.auth.models import User
+from django.db import models
+
+
+# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    date_of_birth = models.DateField(blank=True, null=True)
+    photo = models.ImageField(upload_to='users/%Y/%m/%d/', blank=True, null=True)
+
+    def str(self):
+        return f'Profile of {self.user.username}'
